162ad98f5f10e3a119f4a51e628e2798dd7b22bb
Andreea-Daniela Ene <andreea.ene@devpadawan.ro>
2017-06-06 15:21:38 +0300

    files added

---
+++ SQL Developer/Populare tabele/ANDREEA_game_category.sql
@@ -0,0 +1,4 @@
+INSERT INTO ANDREEA."game_category" ("category_id", "name", "description") VALUES (1, 'Negotiation', 'Negotiation Description');
+INSERT INTO ANDREEA."game_category" ("category_id", "name", "description") VALUES (2, 'Card Game', 'Card Game Description.');
+INSERT INTO ANDREEA."game_category" ("category_id", "name", "description") VALUES (3, 'Territory Building', 'Territory Building Description.');
+INSERT INTO ANDREEA."game_category" ("category_id", "name", "description") VALUES (4, 'Dice', 'Dice Description.');
